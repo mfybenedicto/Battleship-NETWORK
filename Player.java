498e81adbcfd456630d17ed2272623118935c1d7
mfybenedicto <mark_francis_benedicto@dlsu.edu.ph>
2017-03-11 22:17:43 +0800

    Added some classes

---
+++ Player.java
@@ -0,0 +1,30 @@
+public class Player
+{
+	private Ships[] ships;
+	private int whitePegs;
+	private int redPegs;
+	private Account acc;
+	
+	public Player(Account acc, int w, int r)
+	{
+		this.acc = acc;
+		
+		ships = new Ships[5];
+		for(int i = 0; i < 5; i++){
+			ships[i] = new Ships(i + 2);
+		}
+		
+		whitePegs = w;
+		redPegs = r;
+	}
+	
+	public void commandAttack()
+	{
+		//fires a position
+	}
+	
+	public void commandTransport()
+	{
+		//moves to a position
+	}
+}
